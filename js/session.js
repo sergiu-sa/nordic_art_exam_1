0200fcdd4a4cfba8ee185ca00b4e7eab90ad4abc
Sergiu Sarbu <sergiudsarbu@gmail.com>
2026-06-24 23:07:50 +0200

    feat: persist the signed-in user's name in the session

--- js/session.js
+++ js/session.js
@@ -3,6 +3,7 @@
 
 const ACCESS_TOKEN_KEY = "naa.accessToken";
 const API_KEY_KEY = "naa.apiKey";
+const NAME_KEY = "naa.userName";
 
 export function getToken() {
   return sessionStorage.getItem(ACCESS_TOKEN_KEY);
@@ -12,12 +13,18 @@ export function getApiKey() {
   return sessionStorage.getItem(API_KEY_KEY);
 }
 
-export function setSession({ accessToken, apiKey } = {}) {
+export function getUserName() {
+  return sessionStorage.getItem(NAME_KEY);
+}
+
+export function setSession({ accessToken, apiKey, name } = {}) {
   if (accessToken !== undefined) sessionStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
   if (apiKey !== undefined) sessionStorage.setItem(API_KEY_KEY, apiKey);
+  if (name !== undefined) sessionStorage.setItem(NAME_KEY, name);
 }
 
 export function clearSession() {
   sessionStorage.removeItem(ACCESS_TOKEN_KEY);
   sessionStorage.removeItem(API_KEY_KEY);
+  sessionStorage.removeItem(NAME_KEY);
 }
